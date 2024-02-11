1d4da3b300b4cd4033a337f6cbabe3028ef41fcd
KeshavGorantla <136076786+KeshavGoratla@users.noreply.github.com>
2024-02-11 22:41:45 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+function gotoAdmin(){
+    document.getElementById("admin-button").addEventListener("click", function() {
+        window.location.href = "PropertyList.html";
+    });
+}
+function gotoUser(){
+    document.getElementById("user-button").addEventListener("click", function() {
+        window.location.href = "UserPage.html";
+    });
+}
